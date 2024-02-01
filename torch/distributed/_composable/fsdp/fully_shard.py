4c1ea83fcb9c2bfbb8deac23860919fd1db3cc2d
Andrew Gu <andgu@fb.com>
2024-02-01 15:09:47 -0800

    [no ci][FSDP2] Added mixed precision
    ghstack-source-id: fe54f1032db037c2d929d5c028e8c77d57fed6d4
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/118223

--- torch/distributed/_composable/fsdp/fully_shard.py
+++ torch/distributed/_composable/fsdp/fully_shard.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 from torch.distributed._composable import contract
 from torch.distributed._tensor import DeviceMesh
 
+from ._fsdp_api import MixedPrecisionPolicy
 from ._fsdp_common import FSDPMeshInfo, HSDPMeshInfo
 from ._fsdp_init import (
     _get_device_from_mesh,
@@ -28,6 +29,7 @@ def fully_shard(
     *,
     mesh: Optional[DeviceMesh] = None,
     reshard_after_forward: Union[bool, int] = True,
+    mp_policy: MixedPrecisionPolicy = MixedPrecisionPolicy(),
 ):
     """
     Args:
@@ -50,6 +52,9 @@ def fully_shard(
             - The root FSDP state has its value specially set to ``False`` as a
             heuristic since its parameters would typically be immediately
             all-gathered for backward.
+        mp_policy (MixedPrecisionPolicy): This controls the mixed precision
+            policy, which offers parameter/reduction mixed precision for this
+            module. See :class:`MixedPrecisionPolicy` for details.
     """
     if isinstance(module, (nn.ModuleList, nn.ModuleDict)):
         raise ValueError(
@@ -68,14 +73,14 @@ def fully_shard(
     )
 
     state = fully_shard.state(module)
-    state.init(module, device)
+    state.init(module, device, mp_policy)
 
     managed_modules = _get_managed_modules(module)
     params, buffers = _get_managed_states(managed_modules)
     _move_states_to_device(params, buffers, device, mesh_info)
     if params:
         state._fsdp_param_group = FSDPParamGroup(
-            params, module, mesh_info, post_forward_mesh_info, device
+            params, module, mesh_info, post_forward_mesh_info, device, mp_policy
         )
 
     # Place FSDP leftmost for highest priority in the method resolution order
